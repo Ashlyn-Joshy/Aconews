56dc0f3cfb82c8fe44d61f0a25ba3cd8111bc706
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-09-09 12:10:16 +0530

    tailwind added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Welcome to Aconews</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-green-400">
+      Welcome to Aconews
+    </h1>
+  );
 }
 
 export default App;
